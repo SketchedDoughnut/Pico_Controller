1518fb1c6d202ecac049026d7a05abbae4757e03
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-08-16 20:58:47 -0700

    Update Controller_Boot.py

--- BootFiles/Controller_Boot.py
+++ BootFiles/Controller_Boot.py
@@ -12,7 +12,7 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0xA1, 0x01,  # Collection (Application)
     0x85, 0x04,  # Report ID (4)
     
-    # buttons - 2 bytes
+    # buttons - 2 bytes (16 bits used)
     0x05, 0x09,  #   Usage Page (Button)
     0x19, 0x01,  #   Usage Minimum (Button 1)
     0x29, 0x10,  #   Usage Maximum (Button 16)
@@ -22,7 +22,7 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x95, 0x10,  #   Report Count (16)
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
     
-    # joysticks - 4 bytes
+    # joysticks - 4 bytes (32 bits used)
     0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
     0x15, 0x81,  #   Logical Minimum (-127)
     0x25, 0x7F,  #   Logical Maximum (127)
